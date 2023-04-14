1f299172d484d6dfd429a1ce9762c0b475269719
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-14 14:40:05 -0700

    Create string-to-text-file.ps1

---
+++ src/string-to-text-file.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/env pwsh
+# File header specifies pwsh interpreter location for Linux and MacOS
+
+# Define parameters type/name
+param([string]$p1UserString, [string]$p2FilePath)
+
+# Use cmdlet to prompt user to enter a string.
+# Pipeline operator used to send Read-Host output to Trim method
+$stringVariable = Read-Host "Please enter a string:" | Trim
+
+# Display the args (values of the params) in the console
+Write-Host "File path: $p2FilePath"
+Write-Host "User string: $p1UserString"
+
+# Add-Content -Path $p2FilePath -Value $1UserString
+# Append the user string to the specified file contents
+Add-Content -Path $p2FilePath -Append $1UserString
