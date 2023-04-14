eb304e57df3b5221bd8b2cba9270aef47c19fbb0
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-14 15:49:18 -0700

    Update generate-code_signing-cert.ps1

--- src/generate-code_signing-cert.ps1
+++ src/generate-code_signing-cert.ps1
@@ -1,16 +1,26 @@
 #!/usr/bin/env pwsh
 # File header specifies pwsh interpreter location for Linux and MacOS
 
-param()
+# Define parameters type/name
+param([string]$p1Directory, [string]$p2FileName)
+
+Set-ExecutionPolicy RemoteSigned
 
 # Navigate to script directory
-cd 'C:\path\to\directory
+cd $p1Directory
 
-# Generate code signing certificate to sign script
+# Generate code signing certificate to sign script for 1 year
 New-SelfSignedCertificate -Subject "CN=My Code Signing Certificate" -CertStoreLocation "Cert:\CurrentUser\My" -Type CodeSigningCert -NotAfter (Get-Date).AddYears(1)
+# Console output example:
+# PSParentPath: Microsoft.PowerShell.Security\Certificate::CurrentUser\My
+
+# Thumbprint                                Subject
+# ----------                                -------
+# 1234567890abcdef1234567890abcdef12345678  CN=My Code Signing Certificate
+
 
 # Get the path to the certificate
 Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert
 
 # Sign the script with the thumbprint
-Set-AuthenticodeSignature -FilePath .\scriptName.ps1 -Certificate (Get-ChildItem Cert:\CurrentUser\My\thumbprintHere)
+Set-AuthenticodeSignature -FilePath .\scriptName.ps1 -Certificate (Get-ChildItem Cert:\CurrentUser\My\1234567890abcdef1234567890abcdef12345678)
