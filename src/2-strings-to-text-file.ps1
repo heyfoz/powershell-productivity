f05d9502cea045c3a674b256536911d7f75df5c6
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-14 16:16:05 -0700

    Update 2-strings-to-text-file.ps1

--- src/2-strings-to-text-file.ps1
+++ src/2-strings-to-text-file.ps1
@@ -4,7 +4,7 @@
 # 2 Strings to Text File PowerShell Script
 
 # Define parameters type/name
-param([string]$p1UserString, [string]$p2FilePath)
+Param([string]$p1UserString, [string]$p2FilePath)
 
 # Use cmdlet to prompt user to enter a string.
 # Pipeline operator used to send Read-Host output to Trim method
@@ -16,4 +16,4 @@ Write-Host "User string: $p1UserString"
 
 # Add-Content -Path $p2FilePath -Value $1UserString
 # Append the user string to the specified file contents
-Add-Content -Path $p2FilePath -Append $1UserString
+Add-Content -Path $p2FilePath -Append $p1UserString
