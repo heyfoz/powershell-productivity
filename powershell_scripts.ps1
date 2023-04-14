2831e9efbc0a0b723b27c25a27f9350afd9093eb
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-14 11:43:13 -0700

    Update powershell_scripts.ps1

--- powershell_scripts.ps1
+++ powershell_scripts.ps1
@@ -1,8 +1,13 @@
 #!/usr/bin/env pwsh
 # File header specifies pwsh interpreter for Linux and MacOS
 
-# Windows PC: Copy All Directory File Names
-1. Open PowerShell as administrator (Ctrl + Shift + Enter/click).
-2. Enter cd path\to\directory.
-3. Enter Get-ChildItem | Select-Object -ExpandProperty Name > filelist.txt.
-4. Open the file in path\to\directory\filelist.txt to copy the light of file names.
+# 1. Windows PC: Copy All Directory File Names
+#Open PowerShell as administrator (Ctrl + Shift + Enter/click)
+
+# 2. Change directory to desired folder
+cd path\to\directory
+
+# 3. Enter command to create text file containing list of files in current directory
+Get-ChildItem | Select-Object -ExpandProperty Name > filelist.txt
+
+# 4. Open the file in path\to\directory\filelist.txt to copy the light of file names
