85670b41277496528f7be418fcb5bba2583b9db2
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-14 14:42:16 -0700

    Update user-string-cmdlet-prompt.ps1

--- src/user-string-cmdlet-prompt.ps1
+++ src/user-string-cmdlet-prompt.ps1
@@ -3,5 +3,8 @@
 
 # Use cmdlet to prompt user to enter a string.
 # Pipeline operator used to send Read-Host output to Trim method
-$stringVariable = Read-Host "Please enter a string:" | Trim
+$stringVariable = Read-Host "Please enter the file text content:" | Trim
+$filePathVariable = Read-Host "Please enter the file path:" | Trim
 
+# Call the 
+Invoke-Expression -Command ".\Script2.ps1"
