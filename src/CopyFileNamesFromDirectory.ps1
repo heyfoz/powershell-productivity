3d04c2b0d7cce38a93ec0bd3c0424d892791e214
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-14 14:17:35 -0700

    Update CopyFileNamesFromDirectory.ps1

--- src/CopyFileNamesFromDirectory.ps1
+++ src/CopyFileNamesFromDirectory.ps1
@@ -4,7 +4,7 @@
 
 # 1. Open PowerShell as administrator (Ctrl + Shift + Enter/click)
 # 2. Change directory to desired folder
-cd path\to\directory
+cd 'C:\path\to\directory'
 # 3. Enter command to create text file containing list of files in current directory
 Get-ChildItem | Select-Object -ExpandProperty Name > filelist.txt
 # 4. Open the file in path\to\directory\filelist.txt to copy the light of file names
