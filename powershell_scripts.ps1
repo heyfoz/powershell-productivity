4f47984692981a84234b5e7e2e4718a236f493ef
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-14 11:52:22 -0700

    Update powershell_scripts.ps1

--- powershell_scripts.ps1
+++ powershell_scripts.ps1
@@ -3,11 +3,8 @@
 
 # Windows PC: Copy All Directory File Names
 # 1. Open PowerShell as administrator (Ctrl + Shift + Enter/click)
-
 # 2. Change directory to desired folder
 cd path\to\directory
-
 # 3. Enter command to create text file containing list of files in current directory
 Get-ChildItem | Select-Object -ExpandProperty Name > filelist.txt
-
 # 4. Open the file in path\to\directory\filelist.txt to copy the light of file names
